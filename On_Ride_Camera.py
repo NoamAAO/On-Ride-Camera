80c8390d8a698b7ef1c4b4dd84d457e63303d01b
Noam Segev <noam.segev@incredibuild.com>
2023-06-26 17:35:46 +0300

    Add project files.

---
+++ On_Ride_Camera.py
@@ -0,0 +1,53 @@
+
+import cv2
+import time
+
+# Initialize the camera
+cap = cv2.VideoCapture(0)
+
+# Check if the camera is opened successfully
+if not cap.isOpened():
+    print("Failed to open camera")
+    exit()
+
+# Start the countdown
+countdown_start = time.time()
+countdown_duration = 3
+
+while True:
+    # Calculate the time remaining in the countdown
+    countdown_remaining = countdown_duration - int(time.time() - countdown_start)
+    
+    # Read a frame from the camera
+    ret, frame = cap.read()
+    
+    # Display the countdown on the frame
+    cv2.putText(frame, str(countdown_remaining), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    
+    # Display the frame
+    cv2.imshow('On-Ride Camera', frame)
+    
+    # Break the loop when the countdown is complete
+    if countdown_remaining <= 0:
+        break
+    
+    # Break the loop when the 'q' key is pressed
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Capture a frame after the countdown
+ret, frame = cap.read()
+
+# Check if the frame was captured successfully
+if ret:
+    # Save the frame as an image file
+    output_file = 'on_ride_image.jpg'
+    cv2.imwrite(output_file, frame)
+    print(f"Image captured and saved as {output_file}")
+else:
+    print("Failed to capture frame")
+
+# Release the camera and close the OpenCV window
+cap.release()
+cv2.destroyAllWindows()
+
